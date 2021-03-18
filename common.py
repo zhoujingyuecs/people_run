e9cc2fc02cfe6d541164774e61f44a1ef8c5b833
zhoujingyue <zhoujingyue@ruijie.com.cn>
2021-03-18 21:19:37 +0800

    Equal the oppotunity to the bigger will.
    Signed-off-by: zhoujingyue <zhoujingyue@ruijie.com.cn>

--- common.py
+++ common.py
@@ -24,9 +24,9 @@ def init_pool():
 	init_people = [] # [will, money, stock, init_price, in_move, out_move], will: 1 mean buy, 0 means sell.
 	# Need a small number of money and stock to avoid zero deal trap.
 	for i in range(0, int(PEOPLE_NUM / 2)):
-		init_people.append([0, 0.02, 0.02, 0.2, 1.2, 0.8]) # [0.0, 0.5, 0.2, 1.2, 0.8]
+		init_people.append([0, 0.02, 0.02, 0.2, 1.0, 1.0]) # [0.0, 0.5, 0.2, 1.2, 0.8]
 	for i in range(int(PEOPLE_NUM / 2), PEOPLE_NUM):
-		init_people.append([1, 0.02, 0.02, 0.2, 1.2, 0.8]) # [0.0, 0.5, 0.2, 1.2, 0.8]
+		init_people.append([1, 0.02, 0.02, 0.2, 1.0, 1.0]) # [0.0, 0.5, 0.2, 1.2, 0.8]
 	# Init the pool
 	# The people in the pool means the init state of people and the strategy of people.
 	pool = []
@@ -65,7 +65,6 @@ def load_data():
 	file.close()
 	print('Data len:', len(shse))
 	# print(shse) # close,volume,eob
-
 	# Read the data.
 	price = []
 	volume = []
@@ -101,38 +100,73 @@ def get_the_will(people, date, std_price):
 			sell_will += people[k][STOCK] # They want all their stock be the money.
 	return buy_will, sell_will
 
+# # Enable the possible deal.
+# def fit_the_will(people, date, buy_will, sell_will, std_price):
+# 	# print(buy_will, sell_will)
+# 	# for i in range(len(people)):
+# 	# 	print(people[i])
+# 	fit_will = min(buy_will, sell_will)
+# 	remain_buy_will = fit_will
+# 	remain_sell_will = fit_will
+# 	# Fit people's will
+# 	for k in range(PEOPLE_NUM):
+# 		# If this people want to buy and still have money, and other people want sell, fit the deal.
+# 		if people[k][WILL] == I_WANT_BUY and people[k][MONEY] > 0 and remain_sell_will > 0: 
+# 			# Still have enough people want to sell, fit all this people's will.
+# 			if remain_sell_will > people[k][MONEY] / std_price[date]: 
+# 				people[k][STOCK] = people[k][MONEY] / std_price[date]
+# 				remain_sell_will -= people[k][MONEY] / std_price[date]
+# 				people[k][MONEY] = 0
+# 			# Still have people want to sell but not enough, fit all remain_sell_will.
+# 			if remain_sell_will <= people[k][MONEY] / std_price[date]: 
+# 				people[k][STOCK] = remain_sell_will
+# 				people[k][MONEY] -= remain_sell_will * std_price[date]
+# 				remain_sell_will = 0
+# 		# If this people want to sell and still have stock, and other people want buy, fit the deal.
+# 		if people[k][WILL] == I_WANT_SELL and people[k][STOCK] > 0 and remain_buy_will > 0:
+# 			# Still have enough people want to buy, fit all this people's will.
+# 			if remain_buy_will > people[k][STOCK]: 
+# 				people[k][MONEY] = people[k][STOCK] * std_price[date]
+# 				remain_buy_will -= people[k][STOCK]
+# 				people[k][STOCK] = 0
+# 			# Still have people want to buy but not enough, fit all remain_sell_will.
+# 			if remain_buy_will <= people[k][STOCK]: 
+# 				people[k][MONEY] = remain_buy_will * std_price[date]
+# 				people[k][STOCK] -= remain_buy_will
+# 				remain_buy_will = 0
+
 # Enable the possible deal.
 def fit_the_will(people, date, buy_will, sell_will, std_price):
 	# print(buy_will, sell_will)
+	# for i in range(len(people)):
+	# 	print(people[i])
 	fit_will = min(buy_will, sell_will)
-	remain_buy_will = fit_will
-	remain_sell_will = fit_will
-	# Fit people's will
-	for k in range(PEOPLE_NUM):
-		# If this people want to buy and still have money, and other people want sell, fit the deal.
-		if people[k][WILL] == I_WANT_BUY and people[k][MONEY] > 0 and remain_sell_will > 0: 
-			# Still have enough people want to sell, fit all this people's will.
-			if remain_sell_will > people[k][MONEY] / std_price[date]: 
-				people[k][STOCK] = people[k][MONEY] / std_price[date]
-				remain_sell_will -= people[k][MONEY] / std_price[date]
-				people[k][MONEY] = 0
-			# Still have people want to sell but not enough, fit all remain_sell_will.
-			if remain_sell_will <= people[k][MONEY] / std_price[date]: 
-				people[k][STOCK] = remain_sell_will
-				people[k][MONEY] -= remain_sell_will * std_price[date]
-				remain_sell_will = 0
-		# If this people want to sell and still have stock, and other people want buy, fit the deal.
-		if people[k][WILL] == I_WANT_SELL and people[k][STOCK] > 0 and remain_buy_will > 0:
-			# Still have enough people want to buy, fit all this people's will.
-			if remain_buy_will > people[k][STOCK]: 
+	if fit_will == 0:
+		return
+	# Fit all less will.
+	if buy_will >= sell_will:
+		for k in range(PEOPLE_NUM):
+			if people[k][WILL] == I_WANT_SELL:
 				people[k][MONEY] = people[k][STOCK] * std_price[date]
-				remain_buy_will -= people[k][STOCK]
 				people[k][STOCK] = 0
-			# Still have people want to buy but not enough, fit all remain_sell_will.
-			if remain_buy_will <= people[k][STOCK]: 
-				people[k][MONEY] = remain_buy_will * std_price[date]
-				people[k][STOCK] -= remain_buy_will
-				remain_buy_will = 0
+	if sell_will >= buy_will:
+		for k in range(PEOPLE_NUM):
+			if people[k][WILL] == I_WANT_BUY:
+				people[k][STOCK] = people[k][MONEY] / std_price[date]
+				people[k][MONEY] = 0
+	# The oppotunity of the great will should be dicide equally.
+	if buy_will >= sell_will:
+		ratio = sell_will / buy_will
+		for k in range(PEOPLE_NUM):
+			if people[k][WILL] == I_WANT_BUY:
+				people[k][STOCK] = ratio * people[k][MONEY] / std_price[date]
+				people[k][MONEY] -= ratio * people[k][MONEY]
+	if sell_will >= buy_will:
+		ratio = buy_will / sell_will
+		for k in range(PEOPLE_NUM):
+			if people[k][WILL] == I_WANT_SELL:
+				people[k][MONEY] = ratio * people[k][STOCK] * std_price[date]
+				people[k][STOCK] -= ratio * people[k][STOCK]
 
 # Get the will for each people to buy and sell on tomorrow.
 def move_the_will(people, date, std_price):
@@ -140,14 +174,14 @@ def move_the_will(people, date, std_price):
 		# People think about what tomorrow they want to do.
 		if people[k][WILL] == I_WANT_BUY: # The people want to buy.
 			# When the current price touch the out price in people, they want sell.
-			if (people[k][OUT_MOVE] > 1 and std_price[date] > people[k][INIT_PRICE] * people[k][OUT_MOVE]) or \
-			   (people[k][OUT_MOVE] < 1 and std_price[date] < people[k][INIT_PRICE] * people[k][OUT_MOVE]):
+			if (people[k][OUT_MOVE] >= 1 and std_price[date] > people[k][INIT_PRICE] * people[k][OUT_MOVE]) or \
+			   (people[k][OUT_MOVE] <= 1 and std_price[date] < people[k][INIT_PRICE] * people[k][OUT_MOVE]):
 				people[k][WILL] = I_WANT_SELL
 				people[k][INIT_PRICE] = std_price[date]
 		if people[k][WILL] == I_WANT_SELL: # The people want to sell.
 			# When the current price touch the in price in people, they want buy.
-			if (people[k][IN_MOVE] > 1 and std_price[date] > people[k][INIT_PRICE] * people[k][IN_MOVE]) or \
-			   (people[k][IN_MOVE] < 1 and std_price[date] < people[k][INIT_PRICE] * people[k][IN_MOVE]):
+			if (people[k][IN_MOVE] >= 1 and std_price[date] > people[k][INIT_PRICE] * people[k][IN_MOVE]) or \
+			   (people[k][IN_MOVE] <= 1 and std_price[date] < people[k][INIT_PRICE] * people[k][IN_MOVE]):
 				people[k][WILL] = I_WANT_BUY
 				people[k][INIT_PRICE] = std_price[date]
 
